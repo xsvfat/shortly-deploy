8af0c6a7be082340fb5f04d011b2feaacd9aeeeb
Hack Reactor Students <accounts@hackreactor.com>
2016-08-25 16:18:03 -0700

    Implement some login refactoring

--- app/models/user.js
+++ app/models/user.js
@@ -22,23 +22,14 @@ var userSchema = new Schema({
 //       callback(isMatch);
 //     });
 //   },
-//   hashPassword: function() {
-//     var cipher = Promise.promisify(bcrypt.hash);
-//     return cipher(this.get('password'), null, null).bind(this)
-//       .then(function(hash) {
-//         this.set('password', hash);
-//       });
-//   }
-// });
 
 userSchema.methods.comparePassword = function(attemptedPassword, callback) {
-  bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
+  bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
     callback(isMatch);
   });
 };
 
 userSchema.methods.hashPassword = function() {
-  console.log('The user document in the method: ', this);
 
   var cipher = Promise.promisify(bcrypt.hash);
   return cipher(this.password, null, null).bind(this)
