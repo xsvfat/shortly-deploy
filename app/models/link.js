138b76aa9a8a415fc3703723503573ec2bc63183
Hack Reactor Students <accounts@hackreactor.com>
2016-08-25 17:00:21 -0700

    Code cleanup

--- app/models/link.js
+++ app/models/link.js
@@ -3,21 +3,6 @@ var db = require('../config');
 var crypto = require('crypto');
 var Schema = mongoose.Schema;
 
-// var Link = db.Model.extend({
-//   tableName: 'urls',
-//   hasTimestamps: true,
-//   defaults: {
-//     visits: 0
-//   },
-//   initialize: function() {
-//     this.on('creating', function(model, attrs, options) {
-//       var shasum = crypto.createHash('sha1');
-//       shasum.update(model.get('url'));
-//       model.set('code', shasum.digest('hex').slice(0, 5));
-//     });
-//   }
-// });
-
 var linkSchema = new Schema({
   id: String,
   url: String,
